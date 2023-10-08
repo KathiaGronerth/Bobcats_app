b98d83848ea2b149b9fe5ff2f64b313a57b2e27d
Kathia <36736900+KathiaGronerth@users.noreply.github.com>
2023-10-07 19:45:43 -0500

    Update LoginRegister.js

--- src/components/LoginRegister.js
+++ src/components/LoginRegister.js
@@ -1,7 +1,29 @@
 import React, { useState } from "react";
 
 const LoginRegister = () => {
+  // State for toggle
   const [isLogin, setIsLogin] = useState(true);
+
+  // State for login
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // State for registration
+  const [fullName, setFullName] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Logic to authenticate the user goes here.
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // Logic to register the user goes here.
+  };
 
   return (
     <div className="login-register">
@@ -21,20 +43,66 @@ const LoginRegister = () => {
       </div>
 
       {isLogin ? (
-        <div className="login-form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button className="submit-btn">Login</button>
-        </div>
+        <form onSubmit={handleLogin} className="login-form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="submit-btn">
+            Login
+          </button>
+        </form>
       ) : (
-        <div className="register-form">
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <button className="submit-btn">Register</button>
-        </div>
+        <form onSubmit={handleRegister} className="register-form">
+          <input
+            type="text"
+            placeholder="Full Name"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={registerEmail}
+            onChange={(e) => setRegisterEmail(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={registerPassword}
+            onChange={(e) => setRegisterPassword(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="submit-btn">
+            Register
+          </button>
+        </form>
       )}
     </div>
   );
