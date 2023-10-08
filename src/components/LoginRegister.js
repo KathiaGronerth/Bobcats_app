1572bd7a849c2c83fcc4f69cb2ad6078bee8b604
Kathia <36736900+KathiaGronerth@users.noreply.github.com>
2023-10-07 20:46:32 -0500

    Update LoginRegister.js adding Car registration

--- src/components/LoginRegister.js
+++ src/components/LoginRegister.js
@@ -16,6 +16,14 @@ const LoginRegister = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [photo, setPhoto] = useState(null);
 
+  // State variables for car details
+  const [hasCar, setHasCar] = useState(false);
+  const [carModel, setCarModel] = useState("");
+  const [carMake, setCarMake] = useState("");
+  const [carColor, setCarColor] = useState("");
+  const [carYear, setCarYear] = useState("");
+  const [licensePlate, setLicensePlate] = useState("");
+
   const handleLogin = (e) => {
     e.preventDefault();
 
@@ -133,6 +141,59 @@ const LoginRegister = () => {
             accept=".jpg"
             onChange={(e) => setPhoto(e.target.files[0])}
           />
+          {/* New Car fields */}
+          <div className="car-question">
+            <label>Do you have a car?</label>
+            <button
+              type="button"
+              onClick={() => setHasCar(true)}
+              className={hasCar === true ? "active" : ""}
+            >
+              Yes
+            </button>
+            <button
+              type="button"
+              onClick={() => setHasCar(false)}
+              className={hasCar === false ? "active" : ""}
+            >
+              No
+            </button>
+          </div>
+
+          {hasCar && (
+            <>
+              <input
+                type="text"
+                placeholder="Car Model"
+                value={carModel}
+                onChange={(e) => setCarModel(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Car Make"
+                value={carMake}
+                onChange={(e) => setCarMake(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Car Color"
+                value={carColor}
+                onChange={(e) => setCarColor(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Car Year"
+                value={carYear}
+                onChange={(e) => setCarYear(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="License Plate"
+                value={licensePlate}
+                onChange={(e) => setLicensePlate(e.target.value)}
+              />
+            </>
+          )}
           <button type="submit" className="submit-btn">
             Register
           </button>
