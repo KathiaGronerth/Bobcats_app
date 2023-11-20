7f42726c32401165973fb16710d5350403204cde
Kathia <36736900+KathiaGronerth@users.noreply.github.com>
2023-11-19 18:28:48 -0600

    Adding createCar in carsReducer.js

--- client/store/carsReducer.js
+++ client/store/carsReducer.js
@@ -4,35 +4,54 @@ const initialState = {
   cars: [],
 };
 
-//Action Types
+// Action Types
 const GOT_CARS = "GOT_CARS";
+const ADD_CAR = "ADD_CAR"; // New action type
 
-//Action Creators
+// Action Creators
 export const getCars = (cars) => ({
   type: GOT_CARS,
   cars,
 });
 
-//Thunks
-export const fetchCars = () => {
-  return async (dispatch) => {
-    try {
-      const { data: cars } = await axios.get("/api/cars");
-      dispatch(getCars(cars));
-    } catch (error) {
-      console.log("fetchCars thunk error", error);
-    }
-  };
+export const addCar = (car) => ({
+  // New action creator
+  type: ADD_CAR,
+  car,
+});
+
+// Thunks
+export const fetchCars = () => async (dispatch) => {
+  try {
+    const { data: cars } = await axios.get("/api/cars");
+    dispatch(getCars(cars));
+  } catch (error) {
+    console.log("fetchCars thunk error", error);
+  }
 };
 
-// Take a look at app/redux/index.js to see where this reducer is
-// added to the Redux store with combineReducers
+export const createCar = (carData) => async (dispatch) => {
+  // New thunk
+  try {
+    const { data: newCar } = await axios.post("/api/cars", carData);
+    dispatch(addCar(newCar));
+  } catch (error) {
+    console.error("Error creating a car", error);
+  }
+};
+
+// Reducer
 export default function cars(state = initialState, action) {
   switch (action.type) {
     case GOT_CARS:
       return {
         ...state,
-        products: action.products,
+        cars: action.cars,
+      };
+    case ADD_CAR: // Handle new car addition
+      return {
+        ...state,
+        cars: [...state.cars, action.car],
       };
     default:
       return state;
