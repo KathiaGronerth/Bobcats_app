7b96aeaed7946b708b8e125222e947f7322fdfe9
Kathia <36736900+KathiaGronerth@users.noreply.github.com>
2023-11-19 20:38:57 -0600

    Update FindRideForm.js

--- client/components/FindRideForm.js
+++ client/components/FindRideForm.js
@@ -1,54 +1,94 @@
 import React, { useState } from "react";
 
 const FindRideForm = () => {
-  // Using dummy data for the fields as initial values
-  const [startLocation, setStartLocation] = useState("New York City");
-  const [destination, setDestination] = useState("Boston");
-  const [dateTime, setDateTime] = useState("2023-11-01T12:00");
-  const [specialNeeds, setSpecialNeeds] = useState("");
+  const [formData, setFormData] = useState({
+    startLocationLatitude: "",
+    startLocationLongitude: "",
+    endLocationLatitude: "",
+    endLocationLongitude: "",
+    date: "2023-11-01", // Default date
+    time: "12:00", // Default time
+    specialNeeds: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle the submission logic here (e.g., call an API or update state)
-    console.log({ startLocation, destination, dateTime, specialNeeds });
+    console.log(formData);
   };
 
   return (
     <div className="find-ride-form">
       <h2>Find a Ride</h2>
       <form onSubmit={handleSubmit}>
+        {/* Latitude and Longitude Inputs */}
         <div className="input-group">
-          <label>Starting Location:</label>
+          <label>Start Location Latitude:</label>
           <input
             type="text"
-            value={startLocation}
-            onChange={(e) => setStartLocation(e.target.value)}
-            placeholder="Enter starting location"
+            name="startLocationLatitude"
+            value={formData.startLocationLatitude}
+            onChange={handleChange}
           />
         </div>
         <div className="input-group">
-          <label>Destination:</label>
+          <label>Start Location Longitude:</label>
           <input
             type="text"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}
-            placeholder="Enter destination"
+            name="startLocationLongitude"
+            value={formData.startLocationLongitude}
+            onChange={handleChange}
           />
         </div>
         <div className="input-group">
-          <label>Date and Time:</label>
+          <label>End Location Latitude:</label>
           <input
-            type="datetime-local"
-            value={dateTime}
-            onChange={(e) => setDateTime(e.target.value)}
+            type="text"
+            name="endLocationLatitude"
+            value={formData.endLocationLatitude}
+            onChange={handleChange}
           />
         </div>
+        <div className="input-group">
+          <label>End Location Longitude:</label>
+          <input
+            type="text"
+            name="endLocationLongitude"
+            value={formData.endLocationLongitude}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Date and Time Inputs */}
+        <div className="input-group">
+          <label>Date:</label>
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-group">
+          <label>Time:</label>
+          <input
+            type="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Special Needs Input */}
         <div className="input-group">
           <label>Special Needs:</label>
           <textarea
             rows="4"
-            value={specialNeeds}
-            onChange={(e) => setSpecialNeeds(e.target.value)}
+            name="specialNeeds"
+            value={formData.specialNeeds}
+            onChange={handleChange}
             placeholder="Specify any special needs or requirements..."
           />
         </div>
