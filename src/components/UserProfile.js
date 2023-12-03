b422a61ea359c8d2ecf025d8af72cea37389ade9
Kathia <36736900+KathiaGronerth@users.noreply.github.com>
2023-12-03 06:59:47 -0600

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -3,28 +3,35 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import CarProfile from "./CarProfile";
-import Reviews from "./Review"; // Assuming you have a Reviews component
+import ReviewsListUser from "./ReviewsList_User";
+import EditUserProfile from "./EditUserProfile";
+
+const default_profile_photo = "/images/default_profile_photo.jpg";
+const default_cover_photo = "/images/default_cover_photo.jpg";
 
 const UserProfile = () => {
   const [userData, setUserData] = useState(null);
-  const [userReviews, setUserReviews] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch user data
     const fetchUserData = async () => {
       try {
-        // const response = await fetch("http://127.0.0.1:8000/api/profile");
-        const access = JSON.parse(sessionStorage.getItem("access"));
+        //You can uncomment these lines
+        // const access = JSON.parse(sessionStorage.getItem("access"));
+        // const response = await fetch("http://127.0.0.1:8000/api/profile", {
+        //   headers: {
+        //     Authorization: `Bearer ${access}`,
+        //     "Content-Type": "application/json",
+        //   },
+        // });
+
+        /* you can commented out these lines to... */
         const response = await fetch(
-          "http://127.0.0.1:8000/api/profile",
-          {
-            headers: {
-              'Authorization': `Bearer ${access}`,
-            "Content-Type": "application/json",
-            }
-          }
+          "https://run.mocky.io/v3/d80c60c3-7f40-44f4-bace-31eaeada02ad"
         );
+        /*...here*/
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -35,33 +42,14 @@ const UserProfile = () => {
       }
     };
 
-    // Fetch reviews
-    const fetchReviews = async () => {
-      try {
-        //const response = await fetch("http://127.0.0.1:8000/api/review");
-        const response = await fetch(
-          "https://run.mocky.io/v3/8aa6b7e3-2bdb-4d5d-93d3-bdbdf4b90d7e"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const reviews = await response.json();
-        setUserReviews(reviews);
-      } catch (e) {
-        setError(e.message);
-      }
-    };
-
     fetchUserData();
-    fetchReviews();
   }, []);
 
   console.log("UserData: ", userData);
-  console.log("UserReviews: ", userReviews);
   console.log("Error: ", error);
 
   const renderStars = (rating) => {
-    const roundedRating = Math.round(rating * 2) / 2; // round to nearest half
+    const roundedRating = Math.round(rating * 2) / 2;
     let stars = "";
     for (let i = 0; i < 5; i++) {
       if (i < Math.floor(roundedRating)) {
@@ -78,93 +66,167 @@ const UserProfile = () => {
   if (error) return <div>Error: {error}</div>;
   if (!userData) return <div>Loading...</div>;
 
+  const calculateYearsSince = (dateString) => {
+    const [year, month, day] = dateString.split("T")[0].split("-").map(Number);
+    const givenDate = new Date(year, month - 1, day);
+    const currentDate = new Date();
+    const yearsDiff = currentDate.getFullYear() - givenDate.getFullYear();
+    if (
+      currentDate.getMonth() < givenDate.getMonth() ||
+      (currentDate.getMonth() === givenDate.getMonth() &&
+        currentDate.getDate() < givenDate.getDate())
+    ) {
+      return yearsDiff - 1;
+    }
+    return yearsDiff;
+  };
+
+  const handleEditProfile = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async (updatedProfile) => {
+    console.log("Saving updated profile:", updatedProfile);
+
+    try {
+      //const access = JSON.parse(sessionStorage.getItem("access"));
+      //const response = await fetch("http://127.0.0.1:8000/api/profile", {
+      // {
+      //   method: "PATCH",
+      //   headers: {
+      //     Authorization: `Bearer ${access}`,
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(updatedProfile),
+      // });
+
+      const response = await fetch(
+        "https://run.mocky.io/v3/f3b4b6f0-fea8-4ce7-b57f-e600d625a6fd",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedProfile),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const updatedData = await response.json();
+      setUserData({ ...userData, profile: updatedData });
+      setIsEditing(false);
+    } catch (error) {
+      console.error("Failed to save profile:", error);
+    }
+  };
+
   return (
     <div className="user-profile-container">
       <div className="cover-photo">
-        {/* <img
-          src={userData.coverphotourl}
+        <img
+          src={userData.cover_photo || default_cover_photo}
           alt="Cover"
           className="cover-photo-img"
+        />
+        {/* <img
+          src={`data:image/jpeg;base64, ${userData.profile.cover_photo}` || default_cover_photo}
+          alt="Profile"
+          className="cover-photo-img"
         /> */}
-        <img src={`data:image/jpeg;base64, ${userData.cover_photo}`} alt="Profile" className="cover-photo-img"/>
         <div className="profile-header">
-          {/* <img
-            src={userData.photourl}
+          <img
+            src={userData.profile_photo || default_profile_photo}
             alt={`${userData.name}'s profile`}
             className="profile-pic"
-          /> */}
-          <img src={`data:image/jpeg;base64, ${userData.profile_photo}`} alt="Profile" className="profile-pic"/>
-        </div>
-      </div>
-      <div className="profile-content">
-        <h1>{userData.name}</h1>
-        {userData.has_car && <div className="driver-title">Driver</div>}
-        <div className="studies">
-          <span>{userData.studies}</span>
-        </div>
-        <div className="speaks">
-          {userData.speaks.length > 0
-            ? userData.speaks.map((language, index) => (
-                <span key={index}>
-                  {language}
-                  {index < userData.speaks.length - 1 ? ", " : ""}
-                </span>
-              ))
-            : userData.speaks}
-        </div>
-      </div>
-      <div className="stats-container">
-        <div className="stats trips">
-          <span className="number">{userData.trips.toLocaleString()}</span>
-          <span className="label">Trips</span>
-        </div>
-        <div className="stats years">
-          <span className="number">{userData.years}</span>
-          <span className="label">Years</span>
-        </div>
-        <div className="stats rating">
-          <span className="number">{renderStars(userData.rating)}</span>
-          <span className="label">Rating</span>
-        </div>
-      </div>
-      <div className="bio-section">
-        <h3 className="bio-label">Bio</h3>
-        <p>{userData.bio}</p>
-      </div>
-
-      <div className="additional-info">
-        <div className="user-description">
-          <h3>From:</h3>
-          <p>{userData.from_location}</p>
-          <h3>Speaks:</h3>
-          <p>{userData.speaks.join(", ")}</p>
-          <h3>Studies:</h3>
-          <p>{userData.studies}</p>
-          {userData.has_car && <CarProfile carData={userData.cars[0]} />}
-        </div>
-        <div className="user-calendar">
-          <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: "prev,next",
-              center: "title",
-              right: "dayGridMonth,dayGridWeek,dayGridDay",
-            }}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={true}
           />
+          {/* <img
+            src={`data:image/jpeg;base64, ${userData.profile.profile_photo}` || default_profile_photo}
+            alt="Profile"
+            className="profile-pic"
+          /> */}
         </div>
       </div>
-      <div className="review">
-        <h3 style={{ marginLeft: "50px" }}>Reviews</h3>
-        {userReviews && (
-          <Reviews reviews={userReviews} onAddReview={setUserReviews} />
-        )}
-      </div>
+      {isEditing ? (
+        <EditUserProfile profile={userData} onSave={handleSave} />
+      ) : (
+        <>
+          <button style={{ marginLeft: "450px" }} onClick={handleEditProfile}>
+            Edit Profile
+          </button>
+          <div className="profile-content">
+            <h1>{userData.name}</h1>
+            {userData.has_car && <div className="driver-title">Driver</div>}
+            {/* <div className="studies">
+              <span>{userData.studies}</span>
+            </div>
+            <div className="speaks">
+              {userData.speaks.length > 0
+                ? userData.speaks.map((language, index) => (
+                    <span key={index}>
+                      {language}
+                      {index < userData.speaks.length - 1 ? ", " : ""}
+                    </span>
+                  ))
+                : userData.speaks}
+            </div> */}
+          </div>
+          <div style={{ padding: "0px" }} className="stats-container">
+            <div className="stats trips">
+              <span className="number">{userData.trips.toLocaleString()}</span>
+              <span className="label">Trips</span>
+            </div>
+            <div className="stats years">
+              <span className="number">
+                {calculateYearsSince(userData.created_at)}
+              </span>
+              <span className="label">Years</span>
+            </div>
+            <div className="stats rating">
+              <span className="number">{renderStars(userData.rating)}</span>
+              <span className="label">Rating</span>
+            </div>
+          </div>
+          <div className="bio-section">
+            <h3 className="bio-label">Bio</h3>
+            <p>{userData.bio}</p>
+          </div>
+
+          <div className="additional-info">
+            <div className="user-description">
+              <h3>From:</h3>
+              <p>{userData.from_location}</p>
+              <h3>Speaks:</h3>
+              <p>{userData.speaks.join(", ")}</p>
+              <h3>Studies:</h3>
+              <p>{userData.studies}</p>
+              {userData.has_car && <CarProfile carData={userData.cars[0]} />}
+            </div>
+            <div className="user-calendar">
+              <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                  left: "prev,next",
+                  center: "title",
+                  right: "dayGridMonth,dayGridWeek,dayGridDay",
+                }}
+                initialView="dayGridMonth"
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                weekends={true}
+              />
+            </div>
+          </div>
+          <div className="review">
+            <h3 style={{ marginLeft: "50px" }}>Reviews</h3>
+            {userData.reviews && <ReviewsListUser reviews={userData.reviews} />}
+          </div>
+        </>
+      )}
     </div>
   );
 };
