ddbdb6c58db94831cf3593c2fdb320a94a3b05b5
Kathia <36736900+KathiaGronerth@users.noreply.github.com>
2023-12-03 07:00:16 -0600

    Update DriverProfile.js

--- src/components/DriverProfile.js
+++ src/components/DriverProfile.js
@@ -3,54 +3,47 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import CarProfile from "./CarProfile";
-import Reviews from "./Review"; // Assuming you have a Reviews component
+import ReviewsListDriver from "./ReviewsList_Driver"; // Assuming you have a Reviews component
+
+const default_profile_photo = "/images/default_profile_photo.jpg";
+const default_cover_photo = "/images/default_cover_photo.jpg";
 
 const DriverProfile = () => {
-  const [userData, setUserData] = useState(null);
-  const [userReviews, setUserReviews] = useState([]);
+  const [rideData, setRideData] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch user data
-    const fetchUserData = async () => {
+    const fetchRideData = async () => {
       try {
-        // const response = await fetch("http://127.0.0.1:8000/api/notrideyet/driver/user");
+        //You can uncomment these lines
+        //const access = JSON.parse(sessionStorage.getItem("access"));
+        //const response = await fetch("http://127.0.0.1:8000/api/ride", {
+        // {
+        //   headers: {
+        //     Authorization: `Bearer ${access}`,
+        //     "Content-Type": "application/json",
+        //   },
+        // });
+
+        /* you can commented out these lines to... */
         const response = await fetch(
-          "https://run.mocky.io/v3/1979ee24-f7b4-4a38-a062-43841f8823c0"
+          "https://run.mocky.io/v3/f3b4b6f0-fea8-4ce7-b57f-e600d625a6fd"
         );
+        /*...here*/
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        setUserData(data);
+        setRideData(data);
       } catch (e) {
         setError(e.message);
       }
     };
-
-    // Fetch reviews
-    const fetchReviews = async () => {
-      try {
-        //const response = await fetch("http://127.0.0.1:8000/api/review");
-        const response = await fetch(
-          "https://run.mocky.io/v3/8aa6b7e3-2bdb-4d5d-93d3-bdbdf4b90d7e"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const reviews = await response.json();
-        setUserReviews(reviews);
-      } catch (e) {
-        setError(e.message);
-      }
-    };
-
-    fetchUserData();
-    fetchReviews();
+    fetchRideData();
   }, []);
 
-  console.log("UserData: ", userData);
-  console.log("UserReviews: ", userReviews);
+  console.log("RideData: ", rideData);
   console.log("Error: ", error);
 
   const renderStars = (rating) => {
@@ -68,70 +61,98 @@ const DriverProfile = () => {
     return stars;
   };
 
+  const calculateYearsSince = (dateString) => {
+    const [year, month, day] = dateString.split("T")[0].split("-").map(Number);
+    const givenDate = new Date(year, month - 1, day);
+    const currentDate = new Date();
+    const yearsDiff = currentDate.getFullYear() - givenDate.getFullYear();
+    if (
+      currentDate.getMonth() < givenDate.getMonth() ||
+      (currentDate.getMonth() === givenDate.getMonth() &&
+        currentDate.getDate() < givenDate.getDate())
+    ) {
+      return yearsDiff - 1;
+    }
+    return yearsDiff;
+  };
+
   if (error) return <div>Error: {error}</div>;
-  if (!userData) return <div>Loading...</div>;
+  if (!rideData) return <div>Loading...</div>;
 
   return (
     <div className="user-profile-container">
       <div className="cover-photo">
         <img
-          src={userData.coverphotourl}
+          src={rideData.profile.cover_photo || default_cover_photo}
           alt="Cover"
           className="cover-photo-img"
         />
+        {/* <img
+          src={`data:image/jpeg;base64, ${rideData.profile.cover_photo}` || default_cover_photo}
+          alt="Profile"
+          className="cover-photo-img"
+        /> */}
         <div className="profile-header">
           <img
-            src={userData.photourl}
-            alt={`${userData.name}'s profile`}
+            src={rideData.profile.profile_photo || default_profile_photo}
+            alt={`${rideData.profile.name}'s profile`}
             className="profile-pic"
           />
+          {/* <img
+          src={`data:image/jpeg;base64, ${rideData.profile.profile_photo}` || default_profile_photo}
+          alt={`${rideData.profile.name}'s profile`}
+          className="profile-pic"
+        /> */}
         </div>
       </div>
       <div className="profile-content">
-        <h1>{userData.name}</h1>
-        {userData.has_car && <div className="driver-title">Driver</div>}
+        <h1>{rideData.profile.name}</h1>
+        {rideData.profile.has_car && <div className="driver-title">Driver</div>}
         <div className="studies">
-          <span>{userData.studies}</span>
+          <span>{rideData.profile.studies}</span>
         </div>
         <div className="speaks">
-          {userData.speaks.length > 0
-            ? userData.speaks.map((language, index) => (
+          {rideData.profile.speaks.length > 0
+            ? rideData.profile.speaks.map((language, index) => (
                 <span key={index}>
                   {language}
-                  {index < userData.speaks.length - 1 ? ", " : ""}
+                  {index < rideData.profile.speaks.length - 1 ? ", " : ""}
                 </span>
               ))
-            : userData.speaks}
+            : rideData.profile.speaks}
         </div>
       </div>
       <div className="stats-container">
         <div className="stats trips">
-          <span className="number">{userData.trips.toLocaleString()}</span>
+          <span className="number">
+            {rideData.profile.trips.toLocaleString()}
+          </span>
           <span className="label">Trips</span>
         </div>
         <div className="stats years">
-          <span className="number">{userData.years}</span>
+          <span className="number">
+            {" "}
+            {calculateYearsSince(rideData.profile.created_at)}
+          </span>
           <span className="label">Years</span>
         </div>
         <div className="stats rating">
-          <span className="number">{renderStars(userData.rating)}</span>
+          <span className="number">{renderStars(rideData.profile.rating)}</span>
           <span className="label">Rating</span>
         </div>
       </div>
       <div className="bio-section">
         <h3 className="bio-label">Bio</h3>
-        <p>{userData.bio}</p>
+        <p>{rideData.profile.bio}</p>
       </div>
 
       <div className="additional-info">
         <div className="user-description">
           <h3>From:</h3>
-          <p>{userData.from_location}</p>
-          <h3>Speaks:</h3>
-          <p>{userData.speaks.join(", ")}</p>
-          <h3>Studies:</h3>
-          <p>{userData.studies}</p>
-          {userData.has_car && <CarProfile carData={userData.cars[0]} />}
+          <p>{rideData.profile.from_location}</p>
+          {rideData.profile.has_car && (
+            <CarProfile carData={rideData.profile.cars[0]} />
+          )}
         </div>
         <div className="user-calendar">
           <FullCalendar
@@ -152,9 +173,7 @@ const DriverProfile = () => {
       </div>
       <div className="review">
         <h3 style={{ marginLeft: "50px" }}>Reviews</h3>
-        {userReviews && (
-          <Reviews reviews={userReviews} onAddReview={setUserReviews} />
-        )}
+        {rideData.reviews && <ReviewsListDriver reviews={rideData.reviews} />}
       </div>
     </div>
   );
